9268ddde5abb9a6a91aca476a89749262c98da6d
Andrea Shtjefni <andreashtjefni14@gmail.com>
2025-07-24 11:51:47 +0200

    Add files via upload

---
+++ database table employees in sql/creating the employees table.sql
@@ -0,0 +1,17 @@
+CREATE TABLE employees (
+EmpID INT NOT NULL,  
+EmpFirstName VARCHAR (150) NOT NULL,  
+EmpLastName VARCHAR (100) NOT NULL,  
+EmpAge INT NOT NULL,  
+EmpPhoneNumber VARCHAR (100) NOT NULL,
+EmpEmail VARCHAR(150) NOT NULL,
+
+PRIMARY KEY (EmpID)); 
+
+
+
+SELECT * FROM employees;
+
+
+
+
