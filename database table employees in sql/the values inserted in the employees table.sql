254df3aff1f60e6721db9eafc635e7110447f6bc
Andrea Shtjefni <andreashtjefni14@gmail.com>
2025-07-24 11:52:13 +0200

    Add files via upload

---
+++ database table employees in sql/the values inserted in the employees table.sql
@@ -0,0 +1,18 @@
+INSERT INTO employees (EmpID, EmpFirstName, EmpLastName, EmpAge, EmpPhoneNumber,EmpEmail) VALUES 
+(124, 'Maya', 'Ellington', 23, 'mellington@email.com','+355 69 887 4412'),
+(116, 'Julian', 'Ortiz', 30, 'jortiz@email.com','+355 68 334 1122'),
+(063, 'Clara', 'Voss', 25, 'cvoss@email.com ',' +355 69 445 7788'),
+(456, 'Elias', 'Murphy', 22, 'emurphy@email.com','+355 68 998 7700'),
+(198, 'Zoe ', 'Kim', 33, 'zkim@email.com','+355 69 223 8899'),
+(357, 'Tobias', 'Renard', 20, 'trenard@email.com','+355 68 667 1100'),
+(083, 'Freya', 'St. James', 24, 'fstjames@email.com','+355 68 771 9933'),
+(467, 'Luca', 'Martins', 23, 'lmartins@email.com','+355 69 998 7733'),
+(546, 'Amira', 'Solis', 28, 'asolis@email.com','+355 68 445 6677'),
+(223, 'Nathaniel', 'Brooks', 29, 'nbrooks@email.com','+355 69 334 9901');
+
+
+SELECT * FROM employees;
+
+
+
+
