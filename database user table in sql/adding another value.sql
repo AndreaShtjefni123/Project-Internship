631990247143c6bb63856b5bf4b971f5dc08d625
Andrea Shtjefni <andreashtjefni14@gmail.com>
2025-07-24 11:41:17 +0200

    Add files via upload

---
+++ database user table in sql/adding another value.sql
@@ -0,0 +1,2 @@
+ALTER TABLE users
+ADD COLUMN emailofuser VARCHAR(150);
---
+++ database user table in sql/creating a users table.sql
@@ -0,0 +1,8 @@
+CREATE TABLE Users
+(  
+UsersID INT NOT NULL,  
+UsersFirstName VARCHAR (150) NOT NULL,  
+UsersLastName VARCHAR (100) NOT NULL,  
+UsersAge INT NOT NULL,  
+UsersPhoneNumber VARCHAR (100) NOT NULL,  
+PRIMARY KEY (UsersID)); 
