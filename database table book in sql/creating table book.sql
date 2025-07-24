942bc841198c9762404e478d0b0528ea620d281f
Andrea Shtjefni <andreashtjefni14@gmail.com>
2025-07-24 11:52:31 +0200

    Add files via upload

---
+++ database table book in sql/creating table book.sql
@@ -0,0 +1,8 @@
+CREATE TABLE BOOK  
+(  
+BookID VARCHAR (7) NOT NULL,  
+BookTitle VARCHAR (40) NOT NULL,  
+BookAuthor CHAR (40) NOT NULL,  
+BookGenre CHAR (20) NOT NULL,  
+BookLanguage CHAR (10) NOT NULL,  
+PRIMARY KEY (BookID)); 
