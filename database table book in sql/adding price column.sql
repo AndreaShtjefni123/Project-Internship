c97fc14ade609b6229ceb2fb711d9372e553b57c
Andrea Shtjefni <andreashtjefni14@gmail.com>
2025-07-29 10:18:55 +0200

    Add files via upload

---
+++ database table book in sql/adding price column.sql
@@ -0,0 +1,23 @@
+UPDATE book SET price = 31.00 WHERE bookid = 154;
+UPDATE book SET price = 19.70 WHERE bookid = 777;
+UPDATE book SET price = 40.69 WHERE bookid = 342;
+UPDATE book SET price = 29.99 WHERE bookid = 409;
+UPDATE book SET price = 35.99 WHERE bookid = 543;
+UPDATE book SET price = 15.50 WHERE bookid = 602;
+UPDATE book SET price = 25.79 WHERE bookid = 156;
+UPDATE book SET price = 30.00 WHERE bookid = 101;
+UPDATE book SET price = 20.55 WHERE bookid = 355;
+UPDATE book SET price = 40.00 WHERE bookid = 111;
+UPDATE book SET price = 33.10 WHERE bookid = 980;
+UPDATE book SET price = 29.79 WHERE bookid = 135;
+UPDATE book SET price = 27.11 WHERE bookid = 9;
+UPDATE book SET price = 38.00 WHERE bookid = 739;
+UPDATE book SET price = 28.15 WHERE bookid = 0;
+UPDATE book SET price = 39.41 WHERE bookid = 629;
+UPDATE book SET price = 25.11 WHERE bookid = 210;
+UPDATE book SET price = 36.29 WHERE bookid = 896;
+UPDATE book SET price = 30.00  WHERE bookid = 889; 
+UPDATE book SET price = 23.00  WHERE bookid = 124;  
+
+
+SELECT * FROM Book;
